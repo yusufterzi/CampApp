2c69c9d67901f88cea8108061fc2c8818fdac807
NVURAL <nurullahvural1@gmail.com>
2022-04-19 23:03:10 +0300

    code refactoring

--- UI/UI/Components/ComplexComponents/YTPageStack/YTPageStackView.swift
+++ UI/UI/Components/ComplexComponents/YTPageStack/YTPageStackView.swift
@@ -45,25 +45,28 @@ private extension YTPageStackView {
     setupConstraints()
   }
   
-  func setupPages() {
-    stackView.removeArrangedSubviews()
-    guard let presenter = presenter else { return }
-    for index in 0...(presenter.itemCount - 1) {
-      let view = UIView()
-      view.backgroundColor = .white.withAlphaComponent(0.5)
-      view.layer.cornerRadius = 3
-      stackView.addArrangedSubview(view)
-      view.snp.makeConstraints {
-        $0.height.equalTo(6)
-        if index == presenter.selectedItemIndex {
-          $0.width.equalTo(25)
-          view.backgroundColor = .white
-        } else {
-          $0.width.equalTo(6)
+    func setupPages() {
+        stackView.removeArrangedSubviews()
+        guard let presenter = presenter else { return }
+        if presenter.itemCount > 0 {
+            for index in 0...(presenter.itemCount - 1) {
+                let view = UIView()
+                view.backgroundColor = .white.withAlphaComponent(0.5)
+                view.layer.cornerRadius = 3
+                stackView.addArrangedSubview(view)
+                view.snp.makeConstraints {
+                    $0.height.equalTo(6)
+                    if index == presenter.selectedItemIndex {
+                        $0.width.equalTo(25)
+                        view.backgroundColor = .white
+                    } else {
+                        $0.width.equalTo(6)
+                    }
+                }
+            }
+            
         }
-      }
     }
-  }
   
   func setupViews() {
     addSubview(stackView)
